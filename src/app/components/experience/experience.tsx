ae88aabad2c16c9d7aa7963d798f5374515ceebe
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2024-01-04 23:09:44 -0500

    updates in the experience section

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,15 @@
+import { SectionTitle } from '../sectionTitle/section-title'
+import './experience.scss'
+
+export function Experience() {
+    return (
+        <div className='experience'>
+            <SectionTitle text='Experience' />
+            <p>Software Developer and Electrical Engineer with experience in electrical projects and technical documentation, searching for a oportunity as junior software developer.</p>
+
+            <div className="experience-time">
+
+            </div>
+        </div>
+    )
+}
