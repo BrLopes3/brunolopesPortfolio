0064a6e233c8e750662eae50bef8adc39329699f
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2024-01-09 11:12:29 -0500

    feat: inclusion of time of experience in the programming languages

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import { CSharpIcon } from '../icons/cSharp-icon'
-import { JavaScriptIcon } from '../icons/javaScript-icon'
 import { SectionTitle } from '../sectionTitle/section-title'
 import './experience.scss'
-import { ReactIcon } from '../icons/react-icon'
-import { PhpIcon } from '../icons/php-icon'
-import { PythonIcon } from '../icons/python-icon'
-import { SwiftIcon } from '../icons/swift-icon'
-import { JavaIcon } from '../icons/java-icon'
+import Image from 'next/image'
+
 
 export function Experience() {
     return (
@@ -16,46 +11,61 @@ export function Experience() {
             <p>Software Developer and Electrical Engineer with experience in electrical projects and technical documentation, searching for a oportunity as junior software developer.</p>
 
             <div className="experience-time">
-                <div>
-                    <CSharpIcon />
+
+                <div className='experience-language'>
+                    <Image src="/c-sharpIcon.png" alt="Csharp-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-2">
+                            <span>2 years</span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <JavaScriptIcon />
+                <div className='experience-language'>
+                    <Image src="/jsIcon.png" alt="JS-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-2">
+                            <span>2 years</span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <ReactIcon />
+                <div className='experience-language'>
+                    <Image src="/reactIcon.png" alt="React-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <PhpIcon />
+                <div className='experience-language'>
+                    <Image src="/phpIcon.png" alt="Php-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <PythonIcon />
+                <div className='experience-language'>
+                    <Image src="/pythonIcon.png" alt="Python-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-3">
+                            <span>3 years</span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <SwiftIcon />
+                <div className='experience-language'>
+                    <Image src="/swiftIcon.png" alt="Swift-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <JavaIcon />
+                <div className='experience-language'>
+                    <Image src="/javaIcon.png" alt="Java-icon" width={50} height={50} priority />
                     <div className="experience-unit">
-                        <div className="experience-measure"></div>
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
                     </div>
                 </div>
                 
