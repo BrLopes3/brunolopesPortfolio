8813176488c078374d791d9507d13ae1323639d4
BrLopes3 <bruno.lopes@lcieducation.net>
2024-02-05 10:06:56 -0500

    inclusion of projects in the website and update in the design

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -9,11 +9,25 @@ export function Projects() {
     return (
         <div className="projects">
             <SectionTitle text='Projects' />
+            {/* <p>Here are some of my personal projects </p> */}
             <div className="projects-container">
-                <div className="project">
-                    <h3>ConjugAction</h3>
-                    <p>IOS application is designed to assist users in conjugating French verbs effortlessly</p>
-                    <a href="https://github.com/BrLopes3/SwiftProject_ConjugationApp.git"><Image src="/conjApp.png" alt="ConjugAction" width={200} height={200} priority /></a>
+                <div className="projects-examples">
+                    <div className="project">
+                        <h3>ConjugAction</h3>
+                        <a href="https://github.com/BrLopes3/SwiftProject_ConjugationApp.git"><Image src="/conjApp.png" alt="ConjugAction" width={200} height={200} priority /><figcaption>IOS application</figcaption></a>
+                    </div>
+                    <div className="project">
+                        <h3>INT Website - Techweek</h3>
+                        <a href="http://intlasalle.com/"><Image src="/INTLogo.png" alt="Intwebsite" width={200} height={200} priority /><figcaption>Nuxt application</figcaption></a>
+                    </div>
+                    <div className="project">
+                        <h3>Windows Multiform</h3>
+                        <a href="https://github.com/BrLopes3/MultiFormApplication.git"><Image src="/multiformLogo.png" alt="Multiform" width={200} height={200} priority /><figcaption>C# application</figcaption></a>
+                    </div>
+                    <div className="project">
+                        <h3>Edit Photos</h3>
+                        <a href="https://github.com/BrLopes3/EditPhotos.git"><Image src="/editPhotoLogo.png" alt="Edit_Photos" width={200} height={200} priority /><figcaption>Python application</figcaption></a>
+                    </div>
                 </div>
             </div>
         </div>
